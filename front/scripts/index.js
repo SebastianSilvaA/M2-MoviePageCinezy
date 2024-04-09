8592acbfe7f174f9266df15654332e8f1ff3d5ed
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-04-09 20:58:10 -0300

    primercommit

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,14 @@
-console.log(tempData);
+const contenedorCard = document.getElementById('cardContainer')
+
+tempData.map((pelicula) => {
+    const cardDiv = document.createElement('div')
+    cardDiv.classList.add('card')
+
+    cardDiv.innerHTML = `<h3> ${pelicula.title} </h1>`
+
+    contenedorCard.appendChild(cardDiv)
+
+})
+
+console.log(contenedorCard)
+
