c2ddcd082a2815aa3eac7550c59064b9b1af8de2
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-04-09 21:44:18 -0300

    tarea1completada

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -4,11 +4,19 @@ tempData.map((pelicula) => {
     const cardDiv = document.createElement('div')
     cardDiv.classList.add('card')
 
-    cardDiv.innerHTML = `<h3> ${pelicula.title} </h1>`
-
+    cardDiv.innerHTML = `<h3> ${pelicula.title} </h1>
+     <p>año: ${pelicula.year} </p>
+    <p>directo: ${pelicula.director} </p>
+    <h1>duracion: ${pelicula.duration} </p>
+    <p>calificacion: ${pelicula.rate} </p>
+    <img src = '${pelicula.poster}'>
+    <p>Genre: ${pelicula.genre}</p>`
+    
     contenedorCard.appendChild(cardDiv)
+
+    cardDiv.classList.add('gato')
 
 })
 
-console.log(contenedorCard)
+
 
