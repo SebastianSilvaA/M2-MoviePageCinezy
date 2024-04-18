71ac3b4436f9ab63121db11f6ef695a2aabd306d
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-04-18 01:31:54 -0300

    17/04

--- front/scripts/handler.js
+++ front/scripts/handler.js
@@ -7,8 +7,9 @@ const renderizado = require("./renderizar");
 
  const obtener = async () => {
     try {
-       const response = await axios.get("https://students-api.up.railway.app/movies");
-       response.map(renderizado)
+       const response = await axios.get("http://localhost:3000/movies/");
+       const data = response.data
+       data.map(renderizado)
     } catch (error) {
         console.log(error.message)
     }
