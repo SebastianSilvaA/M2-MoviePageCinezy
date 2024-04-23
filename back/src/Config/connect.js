69713763a5441cbd4e85e4c4f9a066148a70d55b
SebastianSilvaA <sebastiansilvaaleman1@hotmail.com>
2024-04-23 00:54:04 -0300

    mongooseupd

--- back/src/Config/connect.js
+++ back/src/Config/connect.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const URI =  "mongodb+srv://sebastinsilvaaleman:g4nrsFzyFEJP8XYV@micluster.jafvbnl.mongodb.net/Movie?retryWrites=true&w=majority&appName=Micluster"
+const URI = `${process.env.DB_HOST}+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.jafvbnl.mongodb.net/${process.env.DB}?retryWrites=true&w=majority&appName=Micluster`
+
 const Dbconnect = async () => {
     await mongoose.connect(URI);
 }
